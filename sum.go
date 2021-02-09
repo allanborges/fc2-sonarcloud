911650bef85ac6b27bee98e8139418d7ccbaef43
Allan Borges <allanborges@gmail.com>
2021-02-08 22:35:56 -0300

    adicionado a funcoes de soma

---
+++ sum.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(sum(2,2))
+}
+
+func sum(a int, b int) int {
+	return a + b
+}
+
+func multiply(a int, b int) int {
+	return a * b
+}
+
+func divide(a int, b int) int {
+	return a / b
+}
