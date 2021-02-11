dee09f70fd3d536c1b2dcfd9f32841491a67f88e
Allan Borges <allanborges@gmail.com>
2021-02-10 23:12:29 -0300

    melhorado a cobertura do teste

--- sum_test.go
+++ sum_test.go
@@ -10,3 +10,21 @@ func TestSum(t *testing.T) {
 		t.Error("Deu pala")
 	}
 }
+
+func TestDivide(t *testing.T) {
+
+	result := divide(20, 5)
+
+	if result != 4 {
+		t.Error("Deu Pala")
+	}
+}
+
+func TestMultiply(t *testing.T) {
+
+	result := multiply(2, 3)
+
+	if result != 5 {
+		t.Error("Deu pala")
+	}
+}
